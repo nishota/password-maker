3f4af9a70608a5dca65b12f671502cdd8436a219
Shota Ninomiya <a2038toh7s1124@gmail.com>
2020-01-19 16:37:33 +0900

    テンプレート1出力を実装

--- src/app/password-place/password-place.component.ts
+++ src/app/password-place/password-place.component.ts
@@ -8,6 +8,8 @@ import { PasswordSaverService } from '../password-saver.service';
 })
 export class PasswordPlaceComponent implements OnInit {
   passwords: string[];
+  selectedPassword: string;
+  template = ['先程、お送りした資料に設定したパスワードです。'];
   constructor(private ps: PasswordSaverService) {
     this.passwords = this.ps.passwords;
   }
@@ -19,17 +21,22 @@ export class PasswordPlaceComponent implements OnInit {
     return this.passwords.length === 0;
   }
 
-  select(target: any) {
+  select(target: any): void {
+    target.select();
+    document.execCommand('copy');
+    this.selectedPassword = this.template[0] + '\r\nパスワード: ' + target.value;
+  }
+
+  selectTemplate(target: any): void {
     target.select();
     document.execCommand('copy');
   }
 
-  // TODO ファイルダウンロード
-  download(target: any) {
+  download(target: any): void {
     const pass = this.passwords.join('\r');
     target.href = URL.createObjectURL(
       new Blob([pass], {
-      type: 'text/plain'
-    }));
+        type: 'text/plain'
+      }));
   }
 }
